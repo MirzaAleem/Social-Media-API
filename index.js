87bca2983de4ff83fcbb43bb25bb31e28ce0136b
Mirza Aleem Abbas <aleemabbas31@gmail.com>
2023-09-22 23:37:27 +0530

    saving shared images on server, multer, path

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const app = express();
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/post');
+const multer = require('multer');
+const path = require('path');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -20,6 +23,34 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}, ()=>{
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/images')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  })
+  
+const upload = multer({ storage: storage })
+
+app.post('/api/upload', upload.single("file"), (req, res) => {
+    try {
+        return res.status(200).json("File Uploaded Successfully")
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+});
+
+app.use((req, res, next) => {
+  res.header('Cross-Origin-Resource-Policy', 'cross-origin')
+  res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
+app.use("/public", express.static(path.join(__dirname, "public/")));
 
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
