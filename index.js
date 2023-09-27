fcbb1b34acfc74ead24afdc014a692deb738782f
Mirza Aleem Abbas <aleemabbas31@gmail.com>
2023-09-27 16:30:17 +0530

    cors reconfiguration

--- index.js
+++ index.js
@@ -24,36 +24,39 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/images')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    }
-  })
-  
-const upload = multer({ storage: storage })
-
-app.post('/api/upload', upload.single("file"), (req, res) => {
-    try {
-        return res.status(200).json("File Uploaded Successfully")
-    } catch (error) {
-        return res.status(500).json(error)
-    }
-});
-
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://ayila.netlify.app'); // Allow requests from any origin
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers');
+  res.header("Access-Control-Allow-Credentials", true);
   next();
+});
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/post', postRoute);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage })
+
+app.post('/api/upload', upload.single("file"), (req, res) => {
+  try {
+      return res.status(200).json("File Uploaded Successfully")
+  } catch (error) {
+      return res.status(500).json(error)
+  }
 });
 
 app.use("/public", express.static(path.join(__dirname, "public/")));
 
-app.use('/api/users', userRoute);
-app.use('/api/auth', authRoute);
-app.use('/api/post', postRoute);
 
 app.listen(8000 , ()=>{
     console.log("Backend Server1 Started")
